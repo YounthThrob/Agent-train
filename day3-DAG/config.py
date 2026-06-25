81e5b527b5f4df8dee73bc328f50c8c3c6691dc6
邸志杰 <1379957488@qq.com>
2026-06-25 13:45:30 +0800

    feat：添加配置

---
+++ day3-DAG/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_KEY = os.getenv("API_KEY")
+    BASE_URL = os.getenv("BASE_URL")
+    MODE = os.getenv("MODEL_NAME")
