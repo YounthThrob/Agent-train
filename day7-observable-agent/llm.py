a72483c41995cd0978a5603ee45ba042d481f226
邸志杰 <1379957488@qq.com>
2026-06-30 13:49:37 +0800

    feat：更新LLM

--- day7-observable-agent/llm.py
+++ day7-observable-agent/llm.py
@@ -1,5 +1,9 @@
 from openai import OpenAI
 from config import Config
+import time
+from core.logger import get_logger
+
+logger = get_logger(__name__)
 
 class LLM:
     def __init__(self):
@@ -7,12 +11,30 @@ class LLM:
         self.model = Config.MODE
 
     def chat(self, messages, temperature=0) -> str:
-        response = self.client.chat.completions.create(
-            model=self.model,
-            messages=messages,
-            temperature=temperature
-        )
-        return {
-            "content": response.choices[0].message.content,
-            "token": response.usage.total_tokens if response.usage else 0
-        }
+        start_time = time.time()
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages,
+                temperature=temperature
+            )
+
+            latency = round(time.time() - start_time, 4)
+            token_usage = response.usage.total_tokens if response.usage else 0
+            content = response.choices[0].message.content
+
+            logger.info(
+                f"LLM call success | model: {self.model} | latency: {latency}s | tokens: {token_usage}"
+            )
+            return{
+                "content": content,
+                "latency": latency,
+                "tokens": token_usage,
+                "model": self.model
+            }
+        except Exception as e:
+            latency = round(time.time() - start_time, 4)
+            logger.error(
+                f"LLM call failed | model: {self.model} | latency: {latency}s | error: {str(e)}"
+            )
+            raise
